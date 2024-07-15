aafacfb23856b56282553f1e5cd09b59fa444804
Brodie Pestell <brodie.pestell7@gmail.com>
2024-07-15 22:53:46 +0100

    Add XMLDoc comments

--- BeesSnazzySnips/Results/ApiResult.cs
+++ BeesSnazzySnips/Results/ApiResult.cs
@@ -2,6 +2,12 @@ namespace BeesSnazzySnips.Results;
 
 public static class ApiResult
 {
+    /// <summary>
+    /// Create a successful ApiResult with a value.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>An ApiResult with a status code of Ok and the specified value.</returns>
     public static ApiResult<T> Success<T>(T? value = default)
     {
         return new ApiResult<T>(
@@ -11,6 +17,14 @@ public static class ApiResult
         );
     }
     
+    /// <summary>
+    /// Create a failure ApiResult, with a status code, error message and value. 
+    /// </summary>
+    /// <param name="statusCode"></param>
+    /// <param name="errorMessage"></param>
+    /// <param name="value"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static ApiResult<T> Failure<T>(
         ResultStatusCode statusCode, 
         string? errorMessage = null, 
@@ -23,6 +37,16 @@ public static class ApiResult
         );
     }
     
+    /// <summary>
+    /// Create an ApiResult using an existing ServiceResult.
+    /// Optionally, the error message can be overriden by specifying an error message.
+    ///
+    /// The returned ApiResult may not be of the required type; refer to <see cref="ApiResult{T}.ConvertValue{TAlt}(System.Func{T?,TAlt?})" />.
+    /// </summary>
+    /// <param name="serviceResult"></param>
+    /// <param name="errorMessage"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static ApiResult<T> FromServiceResult<T>(
         ServiceResult<T> serviceResult, 
         string? errorMessage = null)
@@ -46,16 +70,39 @@ public record ApiResult<T>(
     public bool IsSuccess => StatusCode == ResultStatusCode.Ok;
     public bool IsFailure => !IsSuccess;
     
+    /// <summary>
+    /// Converts the current value using the specified mapping function if the value is not null,
+    /// and returns a new ApiResult of the specified type. If the value is null, the result will
+    /// be the default value for the specified type.
+    /// </summary>
+    /// <typeparam name="TAlt">The type of the result after conversion.</typeparam>
+    /// <param name="mapFunc">The function to map the current value to the new type if it is not null.</param>
+    /// <returns>An ApiResult of the new type after applying the mapping function, or the default value if the current value is null.</returns>
     public ApiResult<TAlt> ConvertValueNotNull<TAlt>(Func<T, TAlt?> mapFunc)
     {
         return ConvertValue(mapFunc, _ => default);
     }
     
+    /// <summary>
+    /// Converts the current value using the specified mapping function, 
+    /// regardless of whether the value is null or not, and returns a new ApiResult of the specified type.
+    /// </summary>
+    /// <typeparam name="TAlt">The type of the result after conversion.</typeparam>
+    /// <param name="mapFunc">The function to map the current value to the new type.</param>
+    /// <returns>An ApiResult of the new type after applying the mapping function.</returns>
     public ApiResult<TAlt> ConvertValue<TAlt>(Func<T?, TAlt?> mapFunc)
     {
         return ConvertValue(mapFunc, mapFunc);
     }
 
+    /// <summary>
+    /// Converts the current value using the specified functions based on whether the value is null or not,
+    /// and returns a new ApiResult of the specified type.
+    /// </summary>
+    /// <typeparam name="TAlt">The type of the result after conversion.</typeparam>
+    /// <param name="whenNotNullFunc">The function to map the current value to the new type if it is not null.</param>
+    /// <param name="whenNullFunc">The function to map the current value to the new type if it is null.</param>
+    /// <returns>An ApiResult of the new type after applying the mapping function.</returns>
     public ApiResult<TAlt> ConvertValue<TAlt>(Func<T, TAlt?> whenNotNullFunc, Func<T?, TAlt?> whenNullFunc)
     {
         var mappedValue = Value is not null
--- BeesSnazzySnips/Results/ServiceResult.cs
+++ BeesSnazzySnips/Results/ServiceResult.cs
@@ -5,17 +5,17 @@ namespace BeesSnazzySnips.Results;
 public static class ServiceResult
 {
     /// <summary>
-    /// Create a "Success" service result, with a status code and an optional value.
+    /// Create a successful ServiceResult, with a status code and an optional value.
     /// </summary>
     /// <param name="value"></param>
     /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <returns>A ServiceResult with a status code of Ok and the specified value.</returns>
     public static ServiceResult<T> Success<T>(T? value) => new(
         StatusCode: ResultStatusCode.Ok,
         Value: value);
     
     /// <summary>
-    /// Create a "Failure" service result, with an exception, error message, status code and value.
+    /// Create a failure ServiceResult, with an exception, error message, status code and value.
     /// </summary>
     /// <param name="exception"></param>
     /// <param name="errorMessage"></param>
@@ -36,7 +36,7 @@ public static class ServiceResult
         );
 
     /// <summary>
-    /// Create a "Failure" service result, with an error message, status code and value.
+    /// Create a failure ServiceResult, with an error message, status code and value.
     /// </summary>
     /// <param name="errorMessage"></param>
     /// <param name="statusCode"></param>
@@ -50,13 +50,13 @@ public static class ServiceResult
         Failure(null, errorMessage, statusCode, value);
     
     /// <summary>
-    /// Create a "Failure" service result from an existing service result but using a new value.
-    /// An error message and/or status code can be specified to override the underlying service result's properties.
+    /// Create a failure ServiceResult from an existing ServiceResult but using a new value.
+    /// An error message and/or status code can be specified to override the underlying ServiceResult's properties.
     /// </summary>
-    /// <param name="serviceResult">Service result to be "copied"</param>
-    /// <param name="value">Value of the newly-created service result</param>
-    /// <param name="errorMessage">Override the underlying service result's message</param>
-    /// <param name="statusCode">Override the underlying service result's status code</param>
+    /// <param name="serviceResult">ServiceResult to be "copied"</param>
+    /// <param name="value">Value of the newly-created ServiceResult</param>
+    /// <param name="errorMessage">Override the underlying ServiceResult's message</param>
+    /// <param name="statusCode">Override the underlying ServiceResult's status code</param>
     /// <typeparam name="TAlt"></typeparam>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
@@ -80,17 +80,38 @@ public record ServiceResult<T>(
     public bool IsSuccess => StatusCode == ResultStatusCode.Ok;
     public bool IsFailure => !IsSuccess;
 
+    /// <summary>
+    /// Returns a value that is never null.
+    /// An exception will be thrown if the value is null.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
     public T Get()
     {
         return Value ?? throw new ArgumentNullException(nameof(Value));
     }
 
+    /// <summary>
+    /// Populates the provided variable with the ServiceResult value
+    /// and returns false if the value is null, or true otherwise.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
     public bool TryGet([MaybeNullWhen(false)] out T value)
     {
         value = Value ?? default;
         return Value is not null;
     }
 
+    /// <summary>
+    /// Creates a new ServiceResult using the specified type and value.
+    /// Optionally, the error message and status code can be overriden.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="errorMessage"></param>
+    /// <param name="statusCode"></param>
+    /// <typeparam name="TAlt"></typeparam>
+    /// <returns></returns>
     public ServiceResult<TAlt> PassThroughFail<TAlt>(
         TAlt? value,
         string? errorMessage = null,
